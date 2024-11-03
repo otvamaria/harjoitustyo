e36779cad9b483b311de4f841ec0c04da7d5ead3
otvamaria <maria.otva@myy.haaga-helia.fi>
2024-11-03 16:52:41 +0200

    lisää testejä tehty

--- src/test/java/syksy24/harjoitustyo/RepositoryTests.java
+++ src/test/java/syksy24/harjoitustyo/RepositoryTests.java
@@ -3,19 +3,24 @@ package syksy24.harjoitustyo;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.ui.ConcurrentModel;
+import org.springframework.ui.Model;
 
 import syksy24.harjoitustyo.domain.Viesti;
 import syksy24.harjoitustyo.domain.ViestiRepository;
 import syksy24.harjoitustyo.domain.Henkilo;
+import syksy24.harjoitustyo.domain.HenkiloRepository;
 import syksy24.harjoitustyo.domain.Hevonen;
 import syksy24.harjoitustyo.domain.HevonenRepository;
-import syksy24.harjoitustyo.web.UserDetailServiceImpl;
+import syksy24.harjoitustyo.web.ViestiController;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @SpringBootTest(classes = HarjoitustyoApplication.class)
@@ -29,11 +34,17 @@ public class RepositoryTests {
     @Autowired
     private HevonenRepository hevonenRepository;
 
-    @Autowired 
-    private UserDetailServiceImpl userDetailServiceImpl;
+    @Autowired
+    private HenkiloRepository henkiloRepository;
+
+    @Autowired
+    private ViestiController viestiController;
+
+
+     //Testejä ViestiController:
 
     @Test
-    public void ainakinYhdellaHevosellaViesti() {
+    public void testAinakinYhdellaHevosellaViesti() {
         List<Viesti> viestit = viestiRepository.findAll();
         Map<Hevonen, List<Viesti>> viestitRyhmitys = viestit.stream()
             .collect(Collectors.groupingBy(Viesti::getHevonen));
@@ -43,13 +54,69 @@ public class RepositoryTests {
     }
 
     @Test
-    public void hevonenLoytyy() {
+    public void testHevonenLoytyy() {
         Hevonen tessa = hevonenRepository.findByNimi("Tessa");
         assertThat(tessa).isNotNull(); 
 
         List<Viesti> viestit = viestiRepository.findByHevonen_Nimi(tessa.getNimi());
         assertThat(viestit).isNotEmpty(); 
     }
+
+    @Test
+    public void testNaytaViestiLomakePalauttaaOikeanSivun() {
+        Model model = new ConcurrentModel();
+
+        String viewName = viestiController.naytaViestiLomake(model);
+
+        assertThat(viewName).isEqualTo("lisaaviesti");
+    }
+
+    
+
+    //Testejä HevonenController:
+
+    @Test
+    public void testFindAllHevoset() {
+        List<Hevonen> hevoset = hevonenRepository.findAll();
+
+        assertThat(hevoset).isNotNull();
+        assertThat(hevoset).isNotEmpty(); 
+    }
+
+    @Test
+    public void testFindSuomenhevonen() {
+        List<Hevonen> hevoset = hevonenRepository.findAll();
+
+        assertThat(hevoset).isNotEmpty(); 
+        assertThat(hevoset).anyMatch(hevonen -> "Suomenhevonen".equals(hevonen.getRotu()));
+    }
+
+    //Testejä HenkiloControllerin toimintaan:
+
+    @Test
+    public void testFindAllHenkilot() {
+        List<Henkilo> henkilot = henkiloRepository.findAll();
+
+        assertThat(henkilot).isNotNull();
+        assertThat(henkilot).isNotEmpty();
+    
+    }
+
+    @Test
+    public void testEtsiHenkiloNimenPerusteella() {
+        List<Henkilo> henkilot = henkiloRepository.findAll();
+
+        Optional<Henkilo> haettuHenkilo = henkilot.stream()
+            .filter(henkilo -> "Muttonen".equals(henkilo.getSukunimi()))
+            .findFirst();
+
+        assertThat(haettuHenkilo).isPresent();
+        assertThat(haettuHenkilo.get().getEtunimi()).isEqualTo("Matti");
+    }
+
+    
+
+    
 
        
 }
--- src/test/java/syksy24/harjoitustyo/RestTests.java
+++ src/test/java/syksy24/harjoitustyo/RestTests.java
@@ -1,5 +1,72 @@
 package syksy24.harjoitustyo;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+
+import org.springframework.http.MediaType;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import syksy24.harjoitustyo.domain.Viesti;
+import syksy24.harjoitustyo.domain.ViestiRepository;
+
+
+
+@SpringBootTest
 public class RestTests {
 
+    @Autowired
+    private WebApplicationContext webAppContext;
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ViestiRepository viestiRepository;
+
+    @BeforeEach
+    public void setUp() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(webAppContext).build();
+    }
+
+    @Test
+    public void testHaeKaikkiViestitStatusOk() throws Exception {
+        mockMvc.perform(get("/viestit"))
+                .andExpect(status().isOk()); 
+    }
+
+    @Test
+    public void testHaeKaikkiViestitResponseTypeApplicationJson() throws Exception {
+        mockMvc.perform(get("/viestit"))
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON)) 
+                .andExpect(status().isOk()); 
+    }
+
+    @Test
+    public void testHaeViestiIdllaExists() throws Exception {
+        int existingId = 51; 
+
+        mockMvc.perform(get("/viestit/{id}", existingId))
+            .andExpect(status().isOk()) 
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON)) 
+            .andExpect(jsonPath("$.teksti").value("Muista laittaa Pollelle loimi ulos")); 
 }
+    
+
+   
+    }
+
+    // Voit lisätä lisää testejä viestien tarkastamiseksi...
+
+
+
